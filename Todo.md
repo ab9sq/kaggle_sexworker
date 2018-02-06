09d3ccf8a97330945d3c55c5c8a0b482d5fb0342
Nick Lauerman <nicholas.lauerman@gmail.com>
2018-02-06 17:15:14 -0600

    update of EOD

--- Todo.md
+++ Todo.md
@@ -1,1 +1,7 @@
 To be used to track task relative theproject
+
+Points Rank values equal to "a" suggestion to make zero.
+
+Blank values for looking for needs to be dealt with. psydo codein thougts.R
+
+Blank valuses for location. more pysdo code in thoughts.R (convert to NA?)
--- thoughts.R
+++ thoughts.R
@@ -6,6 +6,17 @@ nrow(subset(worker, subset = User_ID != ""))
 # worker$Points_Rank <- as.numeric(worker$Points_Rank)
 #      deal with value "a" "" and  1 196   1 215   1 437   1 626 see blanks in values
 
+# Conver a values to zero
+
+x <- worker
+x$Points_Rank <- gsub(pattern = "a",
+                      replacement = "0",
+                      x$Points_Rank)
+x$Points_Rank <- gsub(pattern = " ",
+                      replacement = "",
+                      x$Points_Rank)
+x$Points_Rank <- as.integer(x$Points_Rank)
+
 #worker$Number_of_Comments_in_public_forum <- as.numeric(worker$Number_of_Comments_in_public_forum)
 #    deal with "" 1 174   1 275   1 894  2 062
 
@@ -24,3 +35,24 @@ x$Number_of_Comments_in_public_forum <- gsub(pattern = ",",
 x$Number_of_Comments_in_public_forum <- as.numeric(x$Number_of_Comments_in_public_forum)
 
 Z <- subset(x, is.na(Number_of_Comments_in_public_forum))
+
+
+#looking for
+
+def fill_gender_na(row):
+     if row['Sexual_orientation'] == 'Homosexual':
+     if row['Looking_for'] == 'Men':
+     return 'male'
+elif row['Looking_for'] == 'Women':
+     return 'female'
+elif row['Sexual_orientation'] == 'Heterosexual':
+     if row['Looking_for'] == 'Men':
+     return 'female'
+elif row['Looking_for'] == 'Women':
+     return 'male'
+
+return np.nan
+
+# location
+df['Location'].fillna(df['Location'].mode()[0], inplace=True)
+
